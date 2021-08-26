a14e3e4ce113972ef318502dceb48eecbcd8cc86
Vojin Dudic <vojin3.14@gmail.com>
2021-08-26 18:44:48 +0200

    trachea to lung

--- R/data.R
+++ R/data.R
@@ -10,7 +10,7 @@
 #' @description cleaned and ready data frame for lung cancer
 #' @format A list containing two data frames, one for for women and for men
 #' @export
-"trachea"
+"lung"
 
 #' @title prostate
 #' @description cleaned and ready data frame for prostate cancer
